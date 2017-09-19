94c10081f4a2eb809a6467601dd467e6ede987bc
Avinash Ranjan <avinash.ranjan21@gmail.com>
2017-09-20 00:52:04 +0530

    Node class created

--- src/com/devschool/linkedlist/BasicLinkedList.java
+++ src/com/devschool/linkedlist/BasicLinkedList.java
@@ -3,5 +3,39 @@ package com.devschool.linkedlist;
 /**
  * Created by avinash.ranjan on 18/09/17.
  */
-public class BasicLinkedList {
+public class BasicLinkedList<X> {
+
+    private Node first;
+    private Node last;
+
+    public BasicLinkedList() {
+        first = null;
+        last = null;
+    }
+
+
+
+
+    private class Node {
+        private Node nextNode;
+        private X nodeItem;
+
+        public Node(X item) {
+            this.nextNode = null;
+            this.nodeItem = item;
+        }
+
+        public void setNextNode(Node nextNode) {
+            this.nextNode = nextNode;
+        }
+
+        public Node getNextNode (){
+
+            return nextNode;
+        }
+
+        public X getNodeItem(){
+            return nodeItem;
+        }
+    }
 }
