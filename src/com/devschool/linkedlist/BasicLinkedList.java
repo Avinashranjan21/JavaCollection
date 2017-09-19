2c6f884e7210e3eb03e30f0b9e7cdd2666a21579
Avinash Ranjan <avinash.ranjan21@gmail.com>
2017-09-20 01:06:42 +0530

    Basic linkedList add remove added

--- src/com/devschool/linkedlist/BasicLinkedList.java
+++ src/com/devschool/linkedlist/BasicLinkedList.java
@@ -7,13 +7,42 @@ public class BasicLinkedList<X> {
 
     private Node first;
     private Node last;
+    private int nodeCount;
 
     public BasicLinkedList() {
         first = null;
         last = null;
+        nodeCount = 0;
     }
 
+    public void add(X newItem) {
+//        when we are adding item for the first time in linkedList
+        if (first == null) {
+            first = new Node(newItem);
+            last = first;
+        }
+        // if we have item in linkedList then we will create newNode object and put that object to the last node and set last to newLastNode
+        else {
+            Node newLastNode = new Node(newItem);
+            last.setNextNode(newLastNode);
+            last = newLastNode;
+        }
+        nodeCount++;
+    }
 
+    public X remove() {
+        if (first == null) {
+        throw  new IllegalStateException("The linked list is empty and there is no item in the list");
+        }
+        X nodeItem = first.getNodeItem();
+        first = first.getNextNode();
+        nodeCount--;
+        return nodeItem;
+    }
+
+    public int size() {
+        return nodeCount;
+    }
 
 
     private class Node {
@@ -29,12 +58,12 @@ public class BasicLinkedList<X> {
             this.nextNode = nextNode;
         }
 
-        public Node getNextNode (){
+        public Node getNextNode() {
 
             return nextNode;
         }
 
-        public X getNodeItem(){
+        public X getNodeItem() {
             return nodeItem;
         }
     }
