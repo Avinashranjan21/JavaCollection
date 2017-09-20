277c1e92da23e39fd5d584e72da15a80e0e374dc
Avinash Ranjan <avinash.ranjan21@gmail.com>
2017-09-21 01:03:05 +0530

    hashTable class created and test class added

--- src/com/devschool/hash/BasicHashTable.java
+++ src/com/devschool/hash/BasicHashTable.java
@@ -3,17 +3,91 @@ package com.devschool.hash;
 /**
  * Created by avinash.ranjan on 20/09/17.
  */
-public class BasicHashTable<X,Y> {
+public class BasicHashTable<X, Y> {
 
     private HashEntry[] data;
     private int capacity;
+    private int size;
 
     public BasicHashTable(int tableSize) {
         this.capacity = tableSize;
         this.data = new HashEntry[this.capacity];
+        this.size = 0;
     }
 
-    private class HashEntry<X,Y>{
+    public Y get(X key) {
+        int hash = calculateHash(key);
+        if (data[hash] == null) {
+            return null;
+        } else {
+            return (Y) data[hash].getValue();
+        }
+
+    }
+
+    public void put(X key, Y value) {
+        int hash = calculateHash(key);
+        data[hash] = new HashEntry(key, value);
+        size++;
+    }
+
+    public Y delete(X key) {
+        Y value = get(key);
+        if (value != null) {
+            int hash = calculateHash(key);
+            data[hash] = null;
+            size--;
+            hash = (hash + 1) % this.capacity;
+
+            while (data[hash] != null) {
+                HashEntry hashEntry = data[hash];
+                data[hash] = null;
+                put((X) hashEntry.getKey(), (Y) hashEntry.getValue());
+                size--;
+                hash = (hash + 1) % this.capacity;
+
+            }
+        }
+        return value;
+    }
+
+    public boolean hasKey(X key) {
+        int hash = calculateHash(key);
+        if (data[hash] == null) {
+            return false;
+        } else {
+            if (data[hash].getKey().equals(key)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean hasValue(Y value) {
+        for (int i = 0; i < this.capacity; i++) {
+            HashEntry hashEntry = data[i];
+
+            if (hashEntry != null && hashEntry.getValue().equals(value)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    private int calculateHash(X key) {
+        int hash = (key.hashCode() % this.capacity);
+
+        while (data[hash] != null && !data[hash].getKey().equals(key)) {
+            hash = (hash + 1) % this.capacity;
+        }
+        return hash;
+    }
+
+    private class HashEntry<X, Y> {
         private X key;
         private Y value;
 
