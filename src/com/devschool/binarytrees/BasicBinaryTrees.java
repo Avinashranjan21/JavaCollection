5a2da9afd55812fa8e5b2f1cfca9048631252b6b
Avinash Ranjan <avinash.ranjan21@gmail.com>
2017-09-22 01:16:13 +0530

    binaryTree node class done

--- src/com/devschool/binarytrees/BasicBinaryTrees.java
+++ src/com/devschool/binarytrees/BasicBinaryTrees.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 /**
  * Created by avinash.ranjan on 21/09/17.
  */
-public class BasicBinaryTrees<X extends Comparator<X>> {
+public class BasicBinaryTrees<X extends Comparable<X>> {
 
     private Node root;
     private int size;
@@ -14,11 +14,117 @@ public class BasicBinaryTrees<X extends Comparator<X>> {
         this.root = null;
     }
 
-    private int size(){
+    private int size() {
         return size;
     }
 
-    public void add(X item){
+    public void add(X item) {
+        Node node = new Node(item);
+        if (root == null) {
+            this.root = node;
+            System.out.println("Set root :" + node.getItem());
+            this.size++;
+        } else {
+            insert(this.root, node);
+        }
+    }
+
+    public boolean contain(X item) {
+        Node currentNode = getNode(item);
+        if (currentNode != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean delete(X item) {
+        boolean deleted = false;
+        if (this.root == null) {
+            return false;
+        }
+        Node currentNode = getNode(item);
+        if (currentNode != null) {
+            if (currentNode.getLeft() == null && currentNode.getRight() == null) {
+                unLinkNode(currentNode, null);
+                deleted = true;
+            } else if (currentNode.getLeft() == null && currentNode.getRight() != null) {
+                unLinkNode(currentNode, currentNode.getRight());
+                deleted = true;
+            } else if (currentNode.getLeft() != null && currentNode.getRight() == null) {
+                unLinkNode(currentNode, currentNode.getLeft());
+                deleted = true;
+            } else {
+                Node child = currentNode.getLeft();
+                while (child.getLeft() != null && child.getRight() != null) {
+                    child = child.getRight();
+                }
+
+                child.getParent().setRight(null);
+                child.setLeft(currentNode.getLeft());
+                child.setRight(currentNode.getRight());
+
+                unLinkNode(currentNode, child);
+                deleted = true;
+            }
+        }
+        if (deleted){
+            size--;
+        }
+
+        return deleted;
+    }
+
+    private void unLinkNode(Node currentNode, Node newNode) {
+        if (currentNode == this.root) {
+            newNode.setLeft(currentNode.getLeft());
+            newNode.setRight(currentNode.getRight());
+            this.root = newNode;
+        } else if (currentNode.getParent().getRight() == currentNode) {
+            currentNode.getParent().setRight(newNode);
+        } else {
+            currentNode.getParent().setLeft(newNode);
+        }
+    }
+
+    private Node getNode(X item) {
+        Node currentNode = this.root;
+
+        while (currentNode != null) {
+            int value = item.compareTo(currentNode.getItem());
+
+            if (value == 0) {
+                return currentNode;
+            } else if (value < 0) {
+                currentNode = currentNode.getLeft();
+            } else {
+                currentNode = currentNode.getRight();
+            }
+        }
+        return null;
+    }
+
+    private void insert(Node parent, Node child) {
+
+        if (child.getItem().compareTo(parent.getItem()) < 0) {
+
+            if (parent.getLeft() == null) {
+                parent.setLeft(child);
+                child.setParent(parent);
+                this.size++;
+            } else {
+                insert(parent.getLeft(), child);
+            }
+        } else if (child.getItem().compareTo(parent.getItem()) > 0) {
+            if (parent.getRight() == null) {
+                parent.setRight(child);
+                child.setParent(parent);
+                this.size++;
+
+            } else {
+                insert(parent.getRight(), child);
+            }
+        }
 
     }
 
